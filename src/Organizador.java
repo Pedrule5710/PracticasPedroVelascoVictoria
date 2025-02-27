96d5efa0b2db812368e28a0257e344284fa79644
Pedrule5710 <m.r.pedrule@gmail.com>
2025-02-27 10:39:31 +0100

    CLASE ORGANIZADOR AVANCE

--- src/Organizador.java
+++ src/Organizador.java
@@ -1,4 +1,44 @@
-
 public class Organizador {
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String contraseña;
 
-}
+    public Organizador(String nombre, String email, String telefono, String contraseña) {
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
